31950e1cffb09187bcbd2381e55331df22683e64
Emil <emilridulfo@gmail.com>
2020-08-17 16:47:43 +0200

    Changed to more reliable date source

--- Gatherer.py
+++ Gatherer.py
@@ -2,8 +2,9 @@ import bs4
 import requests
 from datetime import datetime
 
+# list of all the months in a year for later indexing
 months = ["januari", "februari", "mars", "april", "maj", "juni",
-          "juli", "augusti", "september", "oktober", "november", "december"]  # list of all the months in a year for later indexing
+          "juli", "augusti", "september", "oktober", "november", "december"]  
 
 
 class Event:  # Event class used by Gatherer class
@@ -19,15 +20,14 @@ class Event:  # Event class used by Gatherer class
         return self.title
 
     def print(self):  # used to print out a formatted version of event information
-        print(f"DATE: {self.date}\n TITLE: {self.title}\n CONTENT: {self.content}\n TEXT: {self.text}\n","-"*50, "\n")
+        print(f"DATE: {self.date}\nTITLE: {self.title}\nCONTENT: {self.content}\nTEXT: {self.text}\n","-"*50, "\n")
 
 
 class Gatherer:  # class that creates objects which can request new event information
     def __init__(self, source):
 
         self.page = requests.get(source)  # the raw page from request
-        # the raw page after being processed in bs4
-        self.soup = bs4.BeautifulSoup(self.page.content, "html.parser")
+        self.soup = bs4.BeautifulSoup(self.page.content, "html.parser") # processed page
         self.events = []  # list containing the events gathered from the internet
 
     def updateEvents(self):  # used to update tthe list of events
@@ -37,8 +37,8 @@ class Gatherer:  # class that creates objects which can request new event inform
         for i, event in enumerate(self.raw_list):
             if len(event.find("strong", {"class": "list-item-heading"}).get_text().split(", ")[0].split(" ")) == 3:
                 split_output = event.find("strong", {"class": "list-item-heading"}).get_text().split(", ")
-                split_date = split_output[0].split(" ")
-
+                split_date = event.find("div", {"class": "published-container"}).get_text().split("\n")[2].split(" ")
+                
                 # DATETIME VARIABLES
                 year = datetime.now().year  # year variable
                 month = months.index(split_date[1])+1  # month variable
@@ -53,7 +53,9 @@ class Gatherer:  # class that creates objects which can request new event inform
                 content = event.find("span", {"class": "content"}).get_text()
                 text = " ".join([paragraph.get_text() for paragraph in event.find("div", {"class": "text-body"}) if paragraph != "\n"])
                 
+                # APPENDS NEW EVENT CREATED
                 self.events.append(Event(date, location, title, content, text))
+                
 
     def getEvents(self):
         return self.events  # returns self.events list
