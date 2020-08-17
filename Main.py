3f6323d10e80cb3aa10dd616b159eda6983a34c6
Emil <emilridulfo@gmail.com>
2020-08-17 17:49:38 +0200

    Can now send messages to pushbullet

--- Main.py
+++ Main.py
@@ -1,14 +1,28 @@
 from Gatherer import Gatherer
+import requests
+import json
 
-def get_contacts(filename):
-    emails = []
-    with open(filename, mode='r', encoding='utf-8') as contacts_file:
-        for a_contact in contacts_file:
-            emails.append(a_contact.split()[1])
-    return emails
+# pushbullet function
+def pushbullet_message(title, body):
+    msg = {"type": "note", "title": title, "body": body}
+    TOKEN = ''
+    resp = requests.post('https://api.pushbullet.com/v2/pushes', 
+                         data=json.dumps(msg),
+                         headers={'Authorization': 'Bearer ' + TOKEN,
+                                  'Content-Type': 'application/json'})
+    if resp.status_code != 200:
+        raise Exception('Error',resp.status_code)
+    else:
+        print ('Message sent') 
+        
+# formats message to later be used in pushbullet call
+def push_Message(index):
+    gatherer.updateEvents()
+    title = gatherer.getEvents()[index].title + ", " + gatherer.getEvents()[index].location
+    body = str(gatherer.getEvents()[index].date) + "\n" + gatherer.getEvents()[index].text
+    pushbullet_message(title, body)
+    
+#Gatherer object with Stockholm specified 
+gatherer = Gatherer("https://polisen.se/aktuellt/polisens-nyheter?requestId=0&lpfm.loc=Stockholm")
 
-gatherer = Gatherer("https://polisen.se/aktuellt/polisens-nyheter?requestId=1597518822273&lpfm.loc=Stockholm")
-
-gatherer.updateEvents()
-for event in gatherer.getEvents():
-    event.print()
+push_Message(0)
