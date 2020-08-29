f77cdfa26222b682e74d97af2c96ca0829f3f928
Emil Ridulfo <emilridulfo@gmail.com>
2020-08-29 22:08:57 +0200

    Improved structure

--- tests/PoliceGathererTests.py
+++ tests/PoliceGathererTests.py
@@ -1,4 +1,5 @@
-from PoliceGatherer import PoliceGatherer.Gatherer
+from PoliceGatherer.Gatherer import Gatherer
+import pytest
 
 gatherer = Gatherer("https://polisen.se/aktuellt/polisens-nyheter?requestId=1597518822273&lpfm.loc=Stockholm")
 
